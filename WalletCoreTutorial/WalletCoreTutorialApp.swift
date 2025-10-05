9b51fd25b8538ec13131d5332e1220cd997adcb7
LeeSuHyeon <tngus0673@naver.com>
2025-10-05 16:20:23 +0900

    WalletCore 패키지 설치

---
+++ WalletCoreTutorial/WalletCoreTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletCoreTutorialApp.swift
+//  WalletCoreTutorial
+//
+//  Created by 이수현 on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WalletCoreTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
